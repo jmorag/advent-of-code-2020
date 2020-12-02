759bfefd64d4ace09d1f18b7e284278e90826a36
Joseph Morag <jm@josephmorag.com>
2020-12-01 21:17:47 -0500

    Day01 solutions

---
+++ src/Days/Day01.hs
@@ -0,0 +1,42 @@
+module Days.Day01 (runDay, Input, OutputA, OutputB, runA, runB) where
+
+import Data.Attoparsec.Text
+
+runDay :: Bool -> String -> IO ()
+runDay = run inputParser partA partB
+
+runA :: Text -> Either String OutputA
+runA input = runPart input inputParser partA
+
+runB :: Text -> Either String OutputB
+runB input = runPart input inputParser partB
+
+------------ PARSER ------------
+inputParser :: Parser Input
+inputParser = sepBy decimal endOfLine
+
+------------ TYPES ------------
+type Input = [Int]
+
+type OutputA = Int
+
+type OutputB = Int
+
+------------ PART A ------------
+partA :: Input -> OutputA
+partA = comboProd 2020 2
+
+------------ PART B ------------
+partB :: Input -> OutputB
+partB = comboProd 2020 3
+
+comboProd :: (Eq a, Num a) => a -> Int -> [a] -> a
+comboProd goal n input = (\case [x] -> x; _ -> error "Expect one working combination") do
+  combo <- combinations n input
+  guard (sum combo == goal)
+  pure (product combo)
+
+combinations :: Int -> [a] -> [[a]]
+combinations 0 _ = [[]]
+combinations n (x : xs) = ((x :) <$> combinations (n - 1) xs) <> combinations n xs
+combinations _ [] = []
---
+++ tests/Days/Day01Spec.hs
@@ -0,0 +1,22 @@
+module Days.Day01Spec (spec) where
+
+import Days.Day01
+import Test
+
+testInput :: Text
+testInput =
+  [r|1721
+979
+366
+299
+675
+1456|]
+
+spec :: Spec
+spec = do
+  describe "Part a" do
+    it "should find the two numbers summing to 2020 and return their product" $
+      runA testInput === 514579
+  describe "Part b" do
+    it "should find the three numbers summing to 2020 and return their product" $
+      runB testInput === 241861950
