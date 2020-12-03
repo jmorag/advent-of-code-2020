1058a27ff7c7ce1eff4b82591fa88da017ba6b2e
Joseph Morag <jm@josephmorag.com>
2020-12-03 11:52:51 -0500

    Day03 solutions

--- src/Days/Day03.hs
+++ src/Days/Day03.hs
@@ -1,5 +1,6 @@
 module Days.Day03 (runDay, Input, OutputA, OutputB, runA, runB) where
 
+import Data.Array
 import Data.Attoparsec.ByteString.Char8
 
 runDay :: Bool -> String -> IO ()
@@ -13,19 +14,42 @@ runB input = runPart input inputParser partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = do
+  let squareParser = Open <$ char '.' <|> Tree <$ char '#'
+  rows@(r : _) <- sepBy1 (many1 squareParser) endOfLine
+  let nRows = length rows
+      nCols = length r
+  pure $ listArray ((0, 0), (nRows - 1, nCols - 1)) (concat rows)
 
 ------------ TYPES ------------
-type Input = Void
+data Square = Open | Tree
+  deriving (Show)
 
-type OutputA = Void
+data Slope = Slope {right :: Int, down :: Int}
 
-type OutputB = Void
+-- 0 based row-major matrix indexing
+type Input = Array (Int, Int) Square
+
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA input = nTrees input (Slope 3 1)
+
+nTrees :: Array (Int, Int) Square -> Slope -> Int
+nTrees grid Slope {..} = go 0 (0, 0)
+  where
+    (_, (rows, cols)) = bounds grid
+    go acc (i, j) = if i > rows then acc else go acc' (i + down, j + right)
+      where
+        acc' = acc + case grid ! (i, j `mod` (cols + 1)) of Tree -> 1; Open -> 0
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB grid =
+  getProduct $
+    foldMap
+      (Product . nTrees grid)
+      [Slope 1 1, Slope 3 1, Slope 5 1, Slope 7 1, Slope 1 2]
--- tests/Days/Day03Spec.hs
+++ tests/Days/Day03Spec.hs
@@ -4,7 +4,18 @@ import Days.Day03
 import Test
 
 testInput :: ByteString
-testInput = [r||]
+testInput =
+  [r|..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#|]
 
 a :: OutputA -> Expectation
 a = (runA testInput ===)
@@ -15,6 +26,15 @@ b = (runB testInput ===)
 spec :: Spec
 spec = do
   describe "Part a" do
-    xit "" $ a undefined
+    it "should encounter 7 trees when traversing the slope by right 3, down 1" $ a 7
   describe "Part b" do
-    xit "" $ b undefined
+    it
+      ( "should encounter 2,7,3,4, and 2 trees when traversing slopes"
+          <> [r|
+- Right 1, down 1.
+- Right 3, down 1. (This is the slope you already checked.)
+- Right 5, down 1.
+- Right 7, down 1.
+- Right 1, down 2.|]
+      )
+      $ b 336
