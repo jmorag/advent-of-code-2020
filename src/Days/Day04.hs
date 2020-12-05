11413f7f1bc4379b1e5b187f7d7a419e647b7c60
Joseph Morag <jm@josephmorag.com>
2020-12-05 14:59:54 -0500

    Finish day 4

--- src/Days/Day04.hs
+++ src/Days/Day04.hs
@@ -1,6 +1,8 @@
 module Days.Day04 (runDay, Input, OutputA, OutputB, runA, runB) where
 
+import Control.Error
 import Data.Attoparsec.ByteString.Char8 as Atto
+import qualified Data.ByteString.Char8 as B
 import qualified Data.Map as M
 
 runDay :: Bool -> String -> IO ()
@@ -16,12 +18,11 @@ runB input = runPart input inputParser partB
 inputParser :: Parser Input
 inputParser = sepBy1 passport endOfLine
   where
+    passport :: Parser Passport
     passport =
-      M.fromList <$> some do
-        key <- Atto.take 3
-        char ':'
-        val <- Atto.takeTill isSpace
-        space
+      fromList <$> some do
+        key <- Atto.take 3 <* char ':'
+        val <- takeTill isSpace <* space
         pure (key, val)
 
 ------------ TYPES ------------
@@ -33,7 +34,7 @@ type Input = [Passport]
 
 type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
@@ -44,4 +45,30 @@ validPassport passport = all (`M.member` passport) requiredKeys
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = length . filter validPassportB
+
+validPassportB :: Passport -> Bool
+validPassportB passport = and [byr, iyr, eyr, hgt, hcl, ecl, pid]
+  where
+    p key parser =
+      case note "missing key" (passport M.!? key)
+        >>= parseOnly (parser *> endOfInput) of
+        Left _ -> False
+        Right _ -> True
+    range low high n = n >= low && n <= high
+    year low high = guard . range low high =<< decimal
+    byr = p "byr" $ year 1920 2002
+    iyr = p "iyr" $ year 2010 2020
+    eyr = p "eyr" $ year 2020 2030
+    hgt = p "hgt" do
+      n <- decimal
+      ("cm" *> guard (range 150 193 n)) <|> ("in" *> guard (range 59 76 n))
+    hcl = p "hcl" do
+      char '#'
+      color <- Atto.take 6
+      guard (B.all (inClass "0-9a-f") color)
+    ecl = p "ecl" do
+      void $ "amb" <|> "blu" <|> "brn" <|> "gry" <|> "grn" <|> "hzl" <|> "oth"
+    pid = p "pid" do
+      n <- Atto.take 9
+      guard (B.all (inClass "0-9") n)
--- tests/Days/Day04Spec.hs
+++ tests/Days/Day04Spec.hs
@@ -17,17 +17,48 @@ ecl:brn pid:760753108 byr:1931
 hgt:179cm
 
 hcl:#cfa07d eyr:2025 pid:166559648
-iyr:2011 ecl:brn hgt:59in|]
+iyr:2011 ecl:brn hgt:59in
+|]
+
+testInputB :: ByteString
+testInputB =
+  [r|eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007
+
+pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+|]
 
 a :: OutputA -> Expectation
 a = (runA testInput ===)
 
 b :: OutputB -> Expectation
-b = (runB testInput ===)
+b = (runB testInputB ===)
 
 spec :: Spec
 spec = do
   describe "Part a" do
-    it "should report two valid passwords" $ a 2
+    it "should report two valid passports" $ a 2
   describe "Part b" do
-    xit "" $ b undefined
+    it "should report four valid passports" $ b 4
