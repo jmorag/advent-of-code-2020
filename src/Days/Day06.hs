8e8a5f465255d27e22b10da54d8bcd7bb7d97464
Joseph Morag <jm@josephmorag.com>
2020-12-06 11:44:30 -0500

    Day 6

--- src/Days/Day06.hs
+++ src/Days/Day06.hs
@@ -1,6 +1,8 @@
 module Days.Day06 (runDay, Input, OutputA, OutputB, runA, runB) where
 
 import Data.Attoparsec.ByteString.Char8
+import qualified Data.Set as S
+import Relude.Extra.Foldable1
 
 runDay :: Bool -> String -> IO ()
 runDay = run inputParser partA partB
@@ -13,19 +15,29 @@ runB input = runPart input inputParser partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = sepBy groupParser endOfLine
+
+groupParser :: Parser [[Char]]
+groupParser = some $ some (satisfy isAlpha_ascii) <* endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [[String]]
 
-type OutputA = Void
+type OutputA = Sum Int
 
-type OutputB = Void
+type OutputB = Sum Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = foldMap (Sum . S.size . fromList . concat)
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = foldMap (Sum . S.size . mkSet)
+  where
+    mkSet :: [String] -> Set Char
+    mkSet [] = S.empty
+    mkSet (g : gs) = intersections (fmap fromList (g :| gs))
+
+intersections :: (Ord a, Foldable1 t) => t (Set a) -> Set a
+intersections = foldl1' S.intersection . toNonEmpty
--- tests/Days/Day06Spec.hs
+++ tests/Days/Day06Spec.hs
@@ -4,7 +4,23 @@ import Days.Day06
 import Test
 
 testInput :: ByteString
-testInput = [r||]
+testInput =
+  [r|abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+|]
 
 a :: OutputA -> Expectation
 a = (runA testInput ===)
@@ -15,6 +31,6 @@ b = (runB testInput ===)
 spec :: Spec
 spec = do
   describe "Part a" do
-    xit "" $ a undefined
+    it "should return 11 total questions to which someone answered yes" $ a 11
   describe "Part b" do
-    xit "" $ b undefined
+    it "should return 6 total questions to which everyone answered yes" $ b 6
