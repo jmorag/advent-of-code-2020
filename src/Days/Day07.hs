89278e9e3ec35b2d4dcb570cde2ecc74049dc140
Joseph Morag <jm@josephmorag.com>
2020-12-07 21:16:56 -0500

    Day 7

--- src/Days/Day07.hs
+++ src/Days/Day07.hs
@@ -1,6 +1,8 @@
 module Days.Day07 (runDay, Input, OutputA, OutputB, runA, runB) where
 
-import Data.Attoparsec.ByteString.Char8
+import Data.Graph.DGraph
+import Data.Graph.Traversal
+import Data.Graph.Types
 
 runDay :: Bool -> String -> IO ()
 runDay = run inputParser partA partB
@@ -13,19 +15,57 @@ runB input = runPart input inputParser partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = concat <$> sepBy edgeParser endOfLine
+
+-- edge goes from container to containee
+edgeParser :: Parser [Arc ByteString Int]
+edgeParser = do
+  container <- bag
+  void "bags contain "
+  containees <- ([] <$ "no other bags") <|> sepBy containeeParser ", "
+  void (char '.')
+  pure $ map (\(n, containee) -> Arc container containee n) containees
+
+containeeParser :: Parser (Int, ByteString)
+containeeParser = do
+  n <- decimal <* space
+  color <- bag
+  void "bag"
+  peekChar' >>= \case 's' -> void "s"; _ -> pass
+  pure (n, color)
+
+word :: Parser ByteString
+word = takeWhile1 isAlpha_ascii
+
+bag :: Parser ByteString
+bag = do
+  adj <- word
+  void space
+  color <- word
+  void space
+  pure (adj <> " " <> color)
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Arc ByteString Int]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA edges =
+  let edges' = map (\(Arc v v' e) -> Arc v' v e) edges
+      graph = fromArcsList edges'
+   in length (dfsVertices graph "shiny gold") - 1 -- don't count starting vertex
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB edges = go 1 "shiny gold" - 1
+  where
+    graph =
+      let g = fromArcsList edges
+       in if isSimple g then g else error "AHHHHHHHHH LOOPS"
+    go n v =
+      let next = reachableAdjacentVertices' graph v
+       in n + n * sum (map (\(_, v', n') -> go n' v') next)
--- tests/Days/Day07Spec.hs
+++ tests/Days/Day07Spec.hs
@@ -4,7 +4,17 @@ import Days.Day07
 import Test
 
 testInput :: ByteString
-testInput = [r||]
+testInput =
+  [r|light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.
+|]
 
 a :: OutputA -> Expectation
 a = (runA testInput ===)
@@ -12,9 +22,22 @@ a = (runA testInput ===)
 b :: OutputB -> Expectation
 b = (runB testInput ===)
 
+testInput2 :: ByteString
+testInput2 =
+  [r|shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.
+|]
+
 spec :: Spec
 spec = do
   describe "Part a" do
-    xit "" $ a undefined
+    it "should return 4 bags which can eventually contain a shiny gold bag" $
+      a 4
   describe "Part b" do
-    xit "" $ b undefined
+    it "a shiny gold bag needs to contain 32 other bags" $ b 32
+    it "second example" $ runB testInput2 === 126
